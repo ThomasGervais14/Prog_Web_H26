da46736b189abeb285f4827ce9b115a3229bf1b6
ThomasGervais14 <thomasger12@outlook.com>
2026-01-19 15:21:21 -0500

    création de la function qui crée le tableau de saveurs

---
+++ Labo5_6/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let saveurs = [
+    {"saveur":"Fraise","prix": "3"},
+    {"saveur":"Chocolat","prix":"4"},
+    {"saveur":"Double Chocolat","prix":"4"}
+]
+
+let commandes = [
+    {"timeStamp":"2025-01-01 18:00:00", "crmglace":"Crème glacé saveur de Fraise."},
+    {"timeStamp":"2025-01-01 17:50:00", "crmglace":"Crème glacé saveur de Chocolat."}
+    ]
+
+let options = [
+    { "option": "Fraise" },
+    { "option": "Chocolat" },
+    { "option": "Double Chocolat" }
+]
+
+const creerSaveur = function(nom,prix,idd){
+    const ligne = document.createElement("tr");
+    const id = document.createElement("td");
+    id.textContent = idd;
+    const nomtab = document.createElement("td");
+    nomtab.textContent = nom;
+    const prixtab = document.createElement("td");
+    prixtab.textContent = prix + "$";
+    const slotSup = document.createElement("td");
+    const suppr =  document.createElement("button");
+    suppr.textContent = "Supprimer";
+
+    /*insertion du bouton supprimer dans son slot */
+    slotSup.append(suppr);
+
+    /*création de la rangée */
+    ligne.append(id,nomtab,prixtab,slotSup);
+    return ligne
+}
+
+/**création du tableau de saveur à partir de saveurs */
+for(let i = 0; i < saveurs.length; i++){
+    const sav = creerSaveur(saveurs[i].saveur, saveurs[i].prix, i+1);
+    document.querySelector("tbody").appendChild(sav);
+}
+
+} );
+
